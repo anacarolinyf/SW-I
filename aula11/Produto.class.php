cd8291e7db8a1b202d431dbb561d86bc7b3d0b66
anacarolinyf <161509042+anacarolinyf@users.noreply.github.com>
2025-05-23 16:54:37 -0300

    Add files via upload

---
+++ aula11/Produto.class.php
@@ -0,0 +1,21 @@
+<?php
+class Produto{
+    private $Nome;
+    private $Preço;
+    private $Quantidade;
+
+    private function __construct($nome_produto,$preco_produto=0,$quantidade_produto)
+          $this-> Nome = $Nome_produto
+          $this-> Preco = $preco_produto
+          $this-> Quantidade = $quantidade_produto
+}
+
+    public function setNome() {
+        return $this->Nome;
+    }
+
+    public function {
+        
+    }
+
+?>
---
+++ aula11/usaProduto.php
@@ -0,0 +1,8 @@
+<?php
+    include_once 'Pessoa.class.php';
+
+    $livro
+
+
+
+?>
