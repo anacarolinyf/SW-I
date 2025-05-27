9f88057c720fac1f4a031df0cec3f3babad2dd8c
anacarolinyf <161509042+anacarolinyf@users.noreply.github.com>
2025-05-26 21:20:44 -0300

    Add files via upload

--- aula11/Produto.class.php
+++ aula11/Produto.class.php
@@ -1,21 +1,56 @@
 <?php
-class Produto{
-    private $Nome;
-    private $Preço;
-    private $Quantidade;
 
-    private function __construct($nome_produto,$preco_produto=0,$quantidade_produto)
-          $this-> Nome = $Nome_produto
-          $this-> Preco = $preco_produto
-          $this-> Quantidade = $quantidade_produto
+class Produto {
+   
+    private $nome;
+    private $preco;
+    private $quantidade;
+
+    public function __construct($nome, $preco, $quantidade) {
+        $this->nome = $nome;
+        $this->preco = $preco;
+        $this->quantidade = $quantidade;
+    }
+
+    public function getNome() {
+        return $this->nome;
+    }
+
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+
+    public function getPreco() {
+        return $this->preco;
+    }
+
+    public function setPreco($preco) {
+        $this->preco = $preco;
+    }
+
+    public function getQuantidade() {
+        return $this->quantidade;
+    }
+
+    public function adicionarEstoque($qtd) {
+        if ($qtd > 0) {
+            $this->quantidade += $qtd;
+        }
+    }
+
+    public function removerEstoque($qtd) {
+        if ($qtd > 0 && $qtd <= $this->quantidade) {
+            $this->quantidade -= $qtd;
+        } else {
+            echo "Quantidade inválida para remoção.<br>";
+        }
+    }
+
+    public function mostrarDetalhes() {
+        echo "Produto: " . $this->nome . "<br>";
+        echo "Preço: R$ " . number_format($this->preco, 2, ',', '.') . "<br>";
+        echo "Quantidade em estoque: " . $this->quantidade . "<br>";
+    }
 }
-
-    public function setNome() {
-        return $this->Nome;
-    }
-
-    public function {
-        
-    }
 
 ?>
